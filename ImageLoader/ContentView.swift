05e1e10b1ab647ca5382ef14fb3fc47e19e4faf4
Duong Q. Nguyen <wan15112001@gmail.com>
2019-12-16 23:06:30 -0800

    Initial Commit

---
+++ ImageLoader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageLoader
+//
+//  Created by Quang Nguyen on 12/16/19.
+//  Copyright © 2019 Duong Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
